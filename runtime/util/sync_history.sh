eb643520785881692e5eaf6506ff998f793ead9f
Adam Montville <adam.montville.sdo@gmail.com>
2025-04-06 09:52:01 -0500

    Updated sync_history.sh

--- runtime/util/sync_history.sh
+++ runtime/util/sync_history.sh
@@ -7,8 +7,12 @@ git pull origin main
 
 # Pull only the historical_bugout_index.csv file from deployment
 echo "Fetching latest historical data from deployment..."
-git checkout deployment   -- data/historical_bugout_index.csv
+git checkout deployment -- data/historical_bugout_index.csv
 
+# Stage the file explicitly
+git add data/historical_bugout_index.csv
+
+# Commit and push changes
 echo "Committing and pushing changes to main..."
 git commit -m "Merged latest historical data from deployment"
 git push origin main
